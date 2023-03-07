5478a7b4a5fafec43bb7c54944e59829c866e578
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-07 15:29:21 +0300

    Задача 7 HARD

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 7 HARD по желанию - идет за 2 необязательных Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает 
+вторую цифру слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
+456111 -> 5
+78 -> 8
+9146548 -> 1
+3 -> нет
+*/
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = num;
+int i = 0;
+int mod = 0;
+if (num == 0)
+{
+    Console.WriteLine("Введите число отличное от нуля");
+}
+else
+{
+    while (num != 0)
+    {
+        num = num / 10;
+        i++;
+    }
+    if (i < 2)
+    {
+        Console.WriteLine("Слишком короткое число");
+    }
+    else
+    {
+        while (i > 1)
+        {
+            if (i == 2)
+            {
+                mod = num2 % 10;
+                Console.Write("Вторая цифра слева: " + Math.Abs(mod));
+            }
+            i = i - 1;
+            num2 = num2 / 10;
+        }
+    }
+}
